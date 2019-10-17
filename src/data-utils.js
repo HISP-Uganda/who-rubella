8c4f4ef995543a568defaf5eceaa794805bc74ec
carapai <olupotcharles@gmail.com>
2019-10-17 08:25:32 +0300

    Fixed some bugs

--- src/data-utils.js
+++ src/data-utils.js
@@ -152,10 +152,134 @@ export const mrDataValues = async (list, ous, period, attributeOptionCombo) => {
         return v && v !== null && v !== undefined
     });
     if (dataValues.length > 0) {
-       const response =  await postAxios(`${baseUrl}/dataValueSets`, { dataValues });
-       console.log(response);
+        await postAxios(`${baseUrl}/dataValueSets`, { dataValues });
     }
     return dataValues;
+}
+
+export const mapEventData = (list, ous) => {
+    const mapper = {
+        "_id": "",
+        "gps1": "",
+        "gps2": "",
+        "_tags": "",
+        "_uuid": "",
+        "today": "",
+        "_notes": "",
+        "agency": "",
+        "region": "",
+        "_edited": "",
+        "_status": "",
+        "endtime": "",
+        "remarks": "",
+        "_version": "",
+        "deviceid": "",
+        "_duration": "",
+        "_xform_id": "",
+        "districts": "",
+        "starttime": "",
+        "subcounty": "",
+        "_attachments": "",
+        "_geolocation": "",
+        "_media_count": "",
+        "_total_media": "",
+        "formhub/uuid": "",
+        "name_village": "",
+        "_submitted_by": "",
+        "date_campaign": "",
+        "meta/instanceID": "",
+        "name_supervisor": "",
+        "_submission_time": "",
+        "_xform_id_string": "",
+        "level_supervision": "",
+        "target_population": "",
+        "_bamboo_dataset_id": "",
+        "_media_all_received": "",
+        "aefi_reported_at_post": "",
+        "ice_packs_conditioned": "miBHyru8TJ1",
+        "vaccination_team_code": "",
+        "mobile_number_supervisor": "",
+        "name_of_facility_visited": "",
+        "post_reaching_its_targets": "jK9ylxSyIoH",
+        "vaccination_post_location": "",
+        "opv_administered_correctly": "",
+        "team_provided_with_transport": "RQm8Dl9TUqn",
+        "opv_vaccinator_making_left_finger": "",
+        "team_receive_poster_and_materials": "HFN5TTWOb7a",
+        "vaccination_team_members_complete": "f33ZZmn4iZQ",
+        "layout_of_vaccination_post_arrange": "kvt42Dq9BYn",
+        "only_syringes_placed_in_safety_box": "gSqZgu4L0U9",
+        "team_provided_with_two_marker_pens": "",
+        "vaccinator_recap_syringe_after_use": "G7HXVfATlYl",
+        "active_searches_done_house_to_house": "OOQpgGgPbTN",
+        "poster_displayed_at_vaccination_post": "q0yYf8GxTdG",
+        "safety_boxes_appropriately_assembled": "ByJFCBOgrLJ",
+        "day_supply_of_mr_vaccines_and_diluents": "UjmNOUwnwNg",
+        "safety_boxes_closed_and_marked_as_used": "V0pVuWh2dSR",
+        "team_provide_health_education_to_parents": "QBretI5lJFW",
+        "vaccine_carrier_have_two_frozen_Icepacks": "",
+        "clear_and_orderly_flow_of_clients_at_post": "zBmkFbB7JJW",
+        "parents_caregivers_reminded_to_continue_ri": "HRVTKKCeMM2",
+        "vaccinator_fill_only_one_syringe_at_a_time": "ffxjb0a2qop",
+        "vaccinator_properly_clean_vaccination_site": "rZvWDKs4UM7",
+        "number_of_safety_boxes_sufficient_for_today": "",
+        "vaccine_vials_being_used_placed_on_foam_pad": "anFZSVQR1qJ",
+        "disposed_syringes_exposed_out_the_safety_box": "i2VqgJqZuVX",
+        "filled_up_safety_box_kept_in_secure_location": "xDPqFOKS0g5",
+        "safety_boxes_filled_more_than_three_quarters": "IyIJW9e2lNW",
+        "ad_syringes_equal_doses_of_injectable_vaccines": "I7PQ0XuJJ9c",
+        "opv_team_have_vaccine_carrier_for_opv_vaccines": "",
+        "vaccinators_accurately_recording_on_tally_sheet": "Y5bCtjO1rwH",
+        "vaccinators_know_plan_for_disposal_safety_boxes": "y9TWI8tnGwG",
+        "vaccination_team_with_gio_style_vaccine_carriers": "",
+        "opv_vaccinator_tallying_opv_tally_sheet_correctly": "",
+        "vaccine_carrier_for_storing_vaccines_and_diluents": "QYAyID1AkIn",
+        "vaccinator_administer_mr_vaccine_at_left_upper_arm": "E4GukBl0L3m",
+        "vaccination_post_supervisor_knows_how_to_manage_aefi": "ASgrwvMHcyz",
+        "adequate_number_aefi_forms/number_aefi_forms_required": "vCTiFoajlK3",
+        "adequate_number_aefi_forms/number_aefi_forms_available": "URjVjhARy1V",
+        "no_reconstitution_syringes_equals_no_mr_vaccines_vials": "g3xCap8xZlS",
+        "adequate_number_pen_markers/number_pen_markers_required": "bKJULmZqPEP",
+        "vaccinator_accurately_draw_and_give_appropriate_mr_dose": "S2qB7SAgsBO",
+        "adequate_number_pen_markers/number_pen_markers_available": "pEG23J2jv52",
+        "adequate_number_plastic_bags/number_plastic_bags_required": "MELrBjIvlFA",
+        "adequate_number_safety_boxes/number_safety_boxes_required": "fwkrtoKlFCZ",
+        "adequate_number_tally_sheets/number_tally_sheets_required": "XEYER7d8FK8",
+        "adequate_number_plastic_bags/number_plastic_bags_available": "ZX5qi0m6P7V",
+        "adequate_number_safety_boxes/number_safety_boxes_available": "h9t0cDwIsLR",
+        "adequate_number_tally_sheets/number_tally_sheets_available": "ulxi3KoUwpa",
+        "vaccinator_administer_mr_vaccine_through_subcutaneous_route": "YVpxcWerDo5",
+        "adequate_number_aefi_tool_kits/number_aefi_tool_kits_required": "vmU9CtdiIYb",
+        "adequate_number_aefi_tool_kits/number_aefi_tool_kits_available": "m2duLA540qc",
+        "community_adequately_informed_of_campaign_dates_purpose_and_age": "t8CD3YQDlXs",
+        "vaccination_post_supervisor_and_vaccinators_know_aefi_to_report": "oUO6OiixRsR",
+        "adequate_number_roll_cotton_wool/number_roll_cotton_wool_required": "Y79DkODWke4",
+        "adequate_number_vials_mr_diluent/number_vials_mr_diluent_required": "tFlUU7X77Xp",
+        "adequate_number_vials_mr_vaccine/number_vials_mr_vaccine_required": "hcCdk0v6SFS",
+        "vaccination_post_supervisor_and_vaccinators_know_info_to_put_aefi": "lHZV7sxNxXj",
+        "vaccines_and_diluents_not_in_use_stored_in_second_vaccine_carrier": "VOHQ6iaNt6c",
+        "adequate_number_roll_cotton_wool/number_roll_cotton_wool_available": "OnMnXbQRKQW",
+        "adequate_number_vials_mr_diluent/number_vials_mr_diluent_available": "hIwunU3MEvV",
+        "adequate_number_vials_mr_vaccine/number_vials_mr_vaccine_available": "ta7W3NGG69X",
+        "adequate_number_auto_disable_syringes/number_auto_disable_syringes_required": "dRobjlxCjrn",
+        "adequate_number_auto_disable_syringes/number_auto_disable_syringes_available": "CPk3WC16omh",
+        "adequate_number_aefi_line_listing_forms/number_aefi_line_listing_forms_required": "",
+        "adequate_number_reconstitution_syringes/number_reconstitution_syringes_required": "qwdOvhK58Vk",
+        "adequate_number_aefi_line_listing_forms/number_aefi_line_listing_forms_available": "",
+        "adequate_number_reconstitution_syringes/number_reconstitution_syringes_available": "K6ZXImOVuQu"
+    }
+
+    const data = _.keys(list).map(k => {
+        const dataElement = mapper[k];
+
+        if (dataElement !== "") {
+            return { dataElement, value: list[k] }
+        }
+        return null;
+    }).filter(v => v !== null);
+    console.log(data);
+
+    return data;
 }
 
 
@@ -345,15 +469,20 @@ export const truncateString = (str, num) => {
     return str.slice(0, num) + '...'
 }
 
-export const searchPosts = async (subCounty, posts,parent='',create=false) => {
+export const searchPosts = async (subCounty, posts, parent = '', create = false) => {
     const baseUrl = getDHIS2Url();
     const openingDate = moment().subtract(1, 'years');
-    if(create){
-        const ou = { shortName: truncateString(subCounty.name, 46), name: subCounty.name, id:subCounty.id, parent: { id: parent }, openingDate };
+
+    let { data: { dataSets } } = await getAxios(`${baseUrl}/metadata.json`, { dataSets: true });
+    let organisationUnits = dataSets.map(dataSet => dataSet.organisationUnits);
+    organisationUnits = _.uniq(_.flatten(organisationUnits))
+
+    if (create) {
+        const ou = { shortName: truncateString(subCounty.name, 46), name: subCounty.name, id: subCounty.id, parent: { id: parent }, openingDate };
         await postAxios(`${baseUrl}/organisationUnits`, ou);
         await postAxios(`${baseUrl}/schemas/organisationUnit`, ou);
-        subCounty = {...subCounty,children:[]}
-        
+        subCounty = { ...subCounty, children: [] }
+
     }
 
     const { children } = subCounty;
@@ -361,9 +490,6 @@ export const searchPosts = async (subCounty, posts,parent='',create=false) => {
     let newOus = [];
 
     try {
-        let { data: { dataSets } } = await getAxios(`${baseUrl}/metadata.json`, { dataSets: true });
-        let organisationUnits = dataSets.map(dataSet => dataSet.organisationUnits);
-        organisationUnits = _.uniq(_.flatten(organisationUnits))
         for (const post of posts) {
             const search = children.find(p => {
                 return post.toLowerCase() === p.name.toLowerCase();
@@ -375,11 +501,11 @@ export const searchPosts = async (subCounty, posts,parent='',create=false) => {
                 const ou = { shortName: truncateString(post, 46), name: post, id, parent: { id: subCounty.id }, openingDate };
                 await postAxios(`${baseUrl}/organisationUnits`, ou);
                 await postAxios(`${baseUrl}/schemas/organisationUnit`, ou);
+                data = { ...data, [String(post).toLowerCase()]: id };
             }
         }
 
         const values = _.values(data);
-
 
         values.forEach(v => {
             const f = organisationUnits.find(f => f.id === v);
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,12 @@
-import { pullOrganisationUnits, searchPosts, mrDataValues, findType, getCOC, opvDataValues } from './data-utils';
+import { pullOrganisationUnits, searchPosts, mrDataValues, findType, getCOC, opvDataValues, mapEventData } from './data-utils';
 import { Client } from '@elastic/elasticsearch';
 import moment from 'moment';
 import { generateUid } from './uid';
 import winston from './winston';
 const uganda = require(`./uganda.json`);
 const districts = require(`./districts_map.json`);
-
-const client = new Client({ node: 'http://213.136.94.124:9200' });
-// const client = new Client({ node: 'http://localhost:9200' });
+// const client = new Client({ node: 'http://213.136.94.124:9200' });
+const client = new Client({ node: 'http://localhost:9200' });
 
 
 export const routes = (app, io) => {
@@ -17,8 +16,11 @@ export const routes = (app, io) => {
             let { list, date_of_results, day_of_results, _id, _version, ...rest } = req.body;
             let ous = {};
             let processedList = [];
-            let { subcounty, districts, region } = rest;
+            let { subcounty, districts, region, other_subcountyvisited } = rest;
             subcounty = subcounty.split('_').join(' ');
+            if (subcounty === 'OTHERS') {
+                subcounty = String(other_subcountyvisited).split('_').join(' ');
+            }
             districts = districts.split('_').join(' ');
             region = region.split('_').join(' ');
             const district = await pullOrganisationUnits(3, districts);
@@ -46,37 +48,37 @@ export const routes = (app, io) => {
                 await mrDataValues(list, ous, moment(date_of_results).format('YYYYMMDD'), day_of_results);
 
                 for (const l of list) {
-                       
-                        const y35 = parseInt(l['list/children_vaccinated/years3_5'],10);
-                        const y614 = parseInt(l['list/children_vaccinated/years6_14'],10);
-                        const y911 = parseInt(l['list/children_vaccinated/months9_11'],10);
-                        const y1224 = parseInt(l['list/children_vaccinated/months12_24'],10);
-                        const total = y35 + y614 + y911 + y1224;
 
-                        const  partialUse = parseInt(l['list/mr_vaccine_usage/no_vials_discarded_due_partial_use'],10);
-                        const contamination = parseInt(l['list/mr_vaccine_usage/no_vials_discarded_due_contamination'],10);
-                        const otheFactors = parseInt(l['list/mr_vaccine_usage/no_vials_discarded_other_factors'],10);
-                        const colorChange = parseInt(l['list/mr_vaccine_usage/no_vials_discarded_due_vvm_color_change'],10);
-                        const unopen = parseInt(l['list/mr_vaccine_usage/no_vaccine_vials_returned_unopened'],10);
+                    const y35 = parseInt(l['list/children_vaccinated/years3_5'], 10);
+                    const y614 = parseInt(l['list/children_vaccinated/years6_14'], 10);
+                    const y911 = parseInt(l['list/children_vaccinated/months9_11'], 10);
+                    const y1224 = parseInt(l['list/children_vaccinated/months12_24'], 10);
+                    const total = y35 + y614 + y911 + y1224;
 
-                    const discarded =partialUse +contamination +otheFactors+colorChange;
+                    const partialUse = parseInt(l['list/mr_vaccine_usage/no_vials_discarded_due_partial_use'], 10);
+                    const contamination = parseInt(l['list/mr_vaccine_usage/no_vials_discarded_due_contamination'], 10);
+                    const otheFactors = parseInt(l['list/mr_vaccine_usage/no_vials_discarded_other_factors'], 10);
+                    const colorChange = parseInt(l['list/mr_vaccine_usage/no_vials_discarded_due_vvm_color_change'], 10);
+                    const unopen = parseInt(l['list/mr_vaccine_usage/no_vaccine_vials_returned_unopened'], 10);
+
+                    const discarded = partialUse + contamination + otheFactors + colorChange;
                     const original = l['list/name_of_post'];
                     const post = String(original).split('_').join(' ').toLowerCase();
 
                     processedList = [...processedList, {
                         ...l,
-                        ['list/children_vaccinated/total']: isNaN(parseInt(total))?0:total,
-                        ['list/mr_vaccine_usage/no_vials_discarded']: isNaN(parseInt(discarded,10))?0:discarded,
+                        ['list/children_vaccinated/total']: isNaN(parseInt(total)) ? 0 : total,
+                        ['list/mr_vaccine_usage/no_vials_discarded']: isNaN(parseInt(discarded, 10)) ? 0 : discarded,
                         ['list/name_of_post']: ous[post] ? ous[post] : original,
-                        ['list/mr_vaccine_usage/no_vials_discarded_due_partial_use']:isNaN(partialUse)?0:partialUse,
-                        ['list/mr_vaccine_usage/no_vials_discarded_due_contamination']:isNaN(contamination)?0:contamination,
-                        ['list/mr_vaccine_usage/no_vials_discarded_other_factors']:isNaN(otheFactors)?0:otheFactors,
-                        ['list/mr_vaccine_usage/no_vials_discarded_due_vvm_color_change']:isNaN(colorChange)?0:colorChange,
-                        ['list/children_vaccinated/months12_24']:isNaN(y35)?0:y35,
-                        ['list/children_vaccinated/years6_14']:isNaN(y614)?0:y614,
-                        ['list/children_vaccinated/months9_11']:isNaN(y911)?0:y911,
-                        ['list/children_vaccinated/months12_24']:isNaN(y1224)?0:y1224,
-                        ['list/mr_vaccine_usage/no_vaccine_vials_returned_unopened']:isNaN(unopen)?0:unopen,
+                        ['list/mr_vaccine_usage/no_vials_discarded_due_partial_use']: isNaN(partialUse) ? 0 : partialUse,
+                        ['list/mr_vaccine_usage/no_vials_discarded_due_contamination']: isNaN(contamination) ? 0 : contamination,
+                        ['list/mr_vaccine_usage/no_vials_discarded_other_factors']: isNaN(otheFactors) ? 0 : otheFactors,
+                        ['list/mr_vaccine_usage/no_vials_discarded_due_vvm_color_change']: isNaN(colorChange) ? 0 : colorChange,
+                        ['list/children_vaccinated/months12_24']: isNaN(y35) ? 0 : y35,
+                        ['list/children_vaccinated/years6_14']: isNaN(y614) ? 0 : y614,
+                        ['list/children_vaccinated/months9_11']: isNaN(y911) ? 0 : y911,
+                        ['list/children_vaccinated/months12_24']: isNaN(y1224) ? 0 : y1224,
+                        ['list/mr_vaccine_usage/no_vaccine_vials_returned_unopened']: isNaN(unopen) ? 0 : unopen,
                         ...rest,
                         subcounty,
                         districts,
@@ -103,8 +105,11 @@ export const routes = (app, io) => {
             let { list, date_of_results, day_of_results, _id, _version, ...rest } = req.body;
             let ous = {};
             let processedList = [];
-            let { subcounty, districts, region } = rest;
+            let { subcounty, districts, region, other_subcountyvisited } = rest;
             subcounty = subcounty.split('_').join(' ');
+            if (subcounty === 'OTHERS') {
+                subcounty = String(other_subcountyvisited).split('_').join(' ');
+            }
             districts = districts.split('_').join(' ');
             region = region.split('_').join(' ');
             const district = await pullOrganisationUnits(3, districts);
@@ -125,25 +130,24 @@ export const routes = (app, io) => {
 
                     for (const l of list) {
 
-                        const  partialUse = parseInt(l['list/no.vials_discarded_due_to/no_vials_discarded_due_partial_use'],10);
-                        const contamination = parseInt(l['list/no.vials_discarded_due_to/no_vials_discarded_due_contamination'],10);
-                        const otheFactors = parseInt(l['list/no.vials_discarded_due_to/no_vials_discarded_other_factors'],10);
-                        const colorChange = parseInt(l['list/no.vials_discarded_due_to/no_vials_discarded_due_vvm_color_change'],10);
-                        
+                        const partialUse = parseInt(l['list/no.vials_discarded_due_to/no_vials_discarded_due_partial_use'], 10);
+                        const contamination = parseInt(l['list/no.vials_discarded_due_to/no_vials_discarded_due_contamination'], 10);
+                        const otheFactors = parseInt(l['list/no.vials_discarded_due_to/no_vials_discarded_other_factors'], 10);
+                        const colorChange = parseInt(l['list/no.vials_discarded_due_to/no_vials_discarded_due_vvm_color_change'], 10);
+
                         const original = l['list/name_of_post'];
                         const post = String(original).split('_').join(' ').toLowerCase();
-                        const discarded =partialUse +contamination +otheFactors+colorChange;
-
+                        const discarded = partialUse + contamination + otheFactors + colorChange;
 
                         processedList = [...processedList, {
                             ...l,
-                            ['list/no.vials_discarded']: isNaN(parseInt(discarded,10))?0:discarded,
+                            ['list/no.vials_discarded']: isNaN(parseInt(discarded, 10)) ? 0 : discarded,
                             ['list/name_of_post']: ous[post] ? ous[post] : original,
 
-                            ['list/no.vials_discarded_due_to/no_vials_discarded_due_partial_use']:isNaN(partialUse)?0:partialUse,
-                            ['list/no.vials_discarded_due_to/no_vials_discarded_due_contamination']:isNaN(contamination)?0:contamination,
-                            ['list/no.vials_discarded_due_to/no_vials_discarded_other_factors']:isNaN(otheFactors)?0:otheFactors,
-                            ['list/no.vials_discarded_due_to/no_vials_discarded_due_vvm_color_change']:isNaN(colorChange)?0:colorChange,
+                            ['list/no.vials_discarded_due_to/no_vials_discarded_due_partial_use']: isNaN(partialUse) ? 0 : partialUse,
+                            ['list/no.vials_discarded_due_to/no_vials_discarded_due_contamination']: isNaN(contamination) ? 0 : contamination,
+                            ['list/no.vials_discarded_due_to/no_vials_discarded_other_factors']: isNaN(otheFactors) ? 0 : otheFactors,
+                            ['list/no.vials_discarded_due_to/no_vials_discarded_due_vvm_color_change']: isNaN(colorChange) ? 0 : colorChange,
                             ...rest,
                             subcounty,
                             districts,
@@ -162,6 +166,55 @@ export const routes = (app, io) => {
             winston.log('error', e.message);
             response = { message: e.message }
         }
+        return res.status(201).send(response);
+    });
+
+
+    app.post('/checklist', async (req, res) => {
+        let response = {};
+        const data = req.body;
+        let rest = data[0]
+        let { subcounty, districts, region, other_subcountyvisited, vaccination_post_location } = rest;
+        subcounty = subcounty.split('_').join(' ');
+        if (subcounty === 'OTHERS') {
+            subcounty = String(other_subcountyvisited).split('_').join(' ');
+        }
+        districts = districts.split('_').join(' ');
+        region = region.split('_').join(' ');
+        const district = await pullOrganisationUnits(3, districts);
+
+        if (district && district.length === 1) {
+            districts = district[0].id
+            region = district[0].parent.id;
+            const subCounties = district[0].children;
+            const searchedSubCounty = subCounties.filter(s => {
+                return String(s.name).toLowerCase().includes(String(subcounty).toLowerCase());
+            });
+            if (searchedSubCounty.length === 1) {
+                const subCounty = searchedSubCounty[0]
+                subcounty = subCounty.id;
+                const posts = [vaccination_post_location];
+                const ous = await searchPosts(subCounty, posts);
+                vaccination_post_location = ous[vaccination_post_location] ? ous[vaccination_post_location] : vaccination_post_location
+                rest = {
+                    ...rest,
+                    subcounty,
+                    districts,
+                    region,
+                    vaccination_post_location
+                }
+
+                const data = mapEventData(rest, ous);
+
+                const { _version, _id, ...event } = rest
+
+
+                const body = [event].flatMap(doc => [{ index: { _index: 'checklist' } }, doc]);
+                const { body: bulkResponse } = await client.bulk({ refresh: true, body });
+                response = bulkResponse;
+            }
+        }
+
         return res.status(201).send(response);
     });
 
@@ -343,6 +396,55 @@ export const routes = (app, io) => {
         return res.status(200).send(bod);
 
     });
+
+    app.get('/checklist', async (req, res) => {
+        let bod = {}
+        const q = findType(req.query.type);
+
+        const calculations = {
+            posts: { cardinality: { field: 'safety_boxes_filled_more_than_three_quarters' } },
+            testers: { count: { field: 'no_reconstitution_syringes_equals_no_mr_vaccines_vials' } }
+        }
+
+        let single = {
+            filter: {
+                match_all: {}
+            },
+            aggs: calculations
+        }
+
+        let final = {
+            size: 0,
+            aggs: {
+                single
+            }
+        }
+        if (q.search && req.query.search) {
+            final = {
+                ...final,
+                query: {
+                    match: {
+                        [q.search]: {
+                            query: req.query.search
+                        }
+                    }
+                }
+            }
+        }
+        try {
+            const { body } = await client.search({
+                "index": 'checklist',
+                body: final
+            });
+            bod = body;
+        } catch (error) {
+            winston.log('error', error.message);
+
+        }
+        return res.status(200).send(bod);
+
+    });
+
 
     app.get('/uganda', async (req, res) => {
         const q = req.query.search
